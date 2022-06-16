8a26c20d7134d4de6483ef6321be52a6ad6b9b93
Mythszj <1953400941@qq.com>
2022-06-16 18:40:34 +0800

    finish_frontend

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -7,10 +7,36 @@ Page({
     console.log(event)
     let state = event.currentTarget.dataset.state
     console.log(state)
+    // 初始化状态，点击后预约开始排队
     if (state == 1) {
       wx.navigateTo({
         url: '../fill/fill',
       });
+    }
+    // 排队状态，能修改信息，取消预约
+    else if (state == 2) {
+      // 状态2需要，两个按钮
+      let btnid = event.currentTarget.dataset.btnid;
+      // 修改充电信息
+      if (btnid == 1) {
+
+      }
+      // 取消预约
+      else if (btn == 2) {
+
+      }
+    }
+    // 叫号状态，能取消预约
+    else if (state == 3) {
+      
+    }
+    // 正在充电状态，能结束充电
+    else if (state == 4) {
+      
+    }
+    // 充电结束，能结账
+    else if (state == 5) {
+      
     }
   },
 
@@ -45,7 +71,7 @@ Page({
     }
 
     let order = wx.getStorageSync('order')
-    order.state = 2;
+    order.state = 5;
     this.setData({
       order
     })
